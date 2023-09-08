5dcccecd6497212422e40bfafe6009306ba0248d
Devalère KAMGUIA TCHUENKAM <devalerek@outlook.fr>
2023-09-08 09:19:34 +0400

    Implementing Concurrency in Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author $ Devalère
+ * Multithreading is different parts of the same program  using different threads.
+ * This can happen on both single CPU or multiple CPUs
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
